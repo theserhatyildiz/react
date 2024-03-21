d5a5c2c5023d882c13b9484973cbb6d29fbf8634
Serhat Yildiz <yildizserhat@yahoo.com>
2024-03-21 14:10:35 +0300

    class 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
+import { useState } from 'react'
 import './App.css'
-import Product from './Product'
-import User from './User'
+// import Product from './Product'
+// import User from './User'
+import Country from './Country'
 
 function App() {
+
+  let [countries,setCountries] = useState(
+    [
+      {name:"China", capital: "Beijing", pop:"1.4B"},
+      {name:"India", capital: "New Delhi" , pop:"1.38B"},
+      {name:"USA", capital: "Washingot DC", pop:"329M"}
+    ]
+    ) 
+
+    function loadNewCountries ()
+    {
+      setCountries(
+        [
+          {name:"Turkiye", capital: "Ankara", pop:"80M"},
+          {name:"Switzerland", capital: "Basel" , pop:"8.7M"},
+          {name:"Netharlands", capital: "Amsterdam", pop:"17.53M"}
+        ]
+        )
+    }
 
   return (
     <div>
     <h1>Main Component</h1>
 
+    <button onClick={loadNewCountries}>Load New Countries</button>
+
     {/* <Product name="iphone 14" price="$2000" specs={{ram:"2gb", screen:"6.7in"}} category={"Smartphones"} />
 
     <Product name="Samsing Pro"price="$2000" specs={{ram:"2gb", screen:"6.7in"}} category={"Smartphones"} /> */}
 
-    <User/>
+    {/* <User/> */}
+
+    {
+      countries.map((country) =>{
+
+        return (
+          <Country name={country.name} capital={country.capital} pop={country.pop}/>
+        )
+
+      })
+    }
 
     </div>
   )
---
+++ src/Country.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function Country(props)
+{
+    return(
+        <div className="country">
+
+            <h1>{props.name}</h1>
+            <h3>{props.capital}</h3>
+            <p>{props.pop}</p>
+
+        </div>
+    )
+}
+
+export default Country;
